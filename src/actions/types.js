38f2caa588c74e0a4509deb86a1d375f2b52983d
GraceOyiza <graceoyiza13@gmail.com>
2021-09-25 19:56:07 +0100

    Create Action Types

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+export const SET_USER = 'SET_USER';
+export const LOGOUT = 'LOGOUT';
+export const SET_READINGS = 'SET_READINGS';
+export const LOADING = 'LOADING';
+export const SET_READING = 'SET_READING';
+export const REQUEST_ERROR = 'REQUEST_ERROR';
