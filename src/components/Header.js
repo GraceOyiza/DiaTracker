baf10cb733df71d121e45c135592b62daabb3145
GraceOyiza <graceoyiza13@gmail.com>
2021-09-28 22:25:58 +0100

    Add Logout Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { clearHeaders } from '../services/common';
 
-const Header = () => (
-  <div className="d-flex align-items-end justify-content-center position-relative header">
-    <Link to="/">
-      <h3 className="heading">DiaTrack</h3>
-    </Link>
-  </div>
-);
+const Header = () => {
+  const handleClick = () => {
+    clearHeaders();
+      <Redirect to="/" />;
+  };
+  return (
+    <div className="d-flex align-items-end justify-content-center position-relative header">
+      <Link to="/">
+        <h3 className="heading">DiaTrack</h3>
+      </Link>
+
+      <button className="btn btn-secondary logout-btn" type="button" onClick={handleClick}>Logout</button>
+    </div>
+  );
+};
 
 export default Header;
--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,4 @@
-export const baseApi = 'http://localhost:3000/api/v1';
+const { REACT_APP_BASE_API } = process.env;
+
+export const baseApi = REACT_APP_BASE_API;
 export const localStorageKey = 'diatrack';
